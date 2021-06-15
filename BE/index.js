a07b815f6e754cafcf86552c713b5f633ab76d5e
DianaSei <diana@chimaera.my>
2021-06-15 17:04:33 +0800

    Calculation of pi

--- BE/index.js
+++ BE/index.js
@@ -5,6 +5,18 @@ app.get("/",function(request,response){
   response.send("Hello World!")
 });
 
+var pi = 3;
+var x = 2;
+
+for (var count = 1; count < 500000; count++) {
+  if (count % 2) {
+    pi = pi + (4/(x * (x+1) * (x+2)));
+  } else {
+    pi = pi - (4/(x * (x+1) * (x+2)));
+  }
+  x += 2;
+}
+
 app.listen(3080, function () {
-  console.log("Started application on port %d", 3080);
+  console.log("Started application on port %d", 3080, pi);
 });
